8af5fe4fdb3e20f11359796fa2843302911d2aaa
myunghan <myunghan@myunghanui-iMac.local>
2022-12-20 13:17:57 +0900

    변수 선언 및 변수 저장

---
+++ src/main/java/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,11 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        int value;
+
+        //int result = value + 10; // error: variable value might not have been initialized
+
+        //System.out.println(result);
+    }
+}
