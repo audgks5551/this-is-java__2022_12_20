2431cdd156e4fdf4d196bcf31559169e75e08130
myunghan <myunghan@myunghanui-iMac.local>
2022-12-20 12:47:26 +0900

    chapter1

---
+++ src/main/java/org/example/Hello.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * @author 이명한
+ */
+
+/*
+장제목: 1장 자바 시작하기
+작성일: 2022.12.20
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
